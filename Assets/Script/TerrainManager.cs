dc72ec134969dea48deabc921aa0681bcebe4d01
김은서 <a55208806@gmail.com>
2024-01-22 18:49:30 +0900

    [Temp] 오늘 한거 커밋
    별거안함

--- Assets/Script/TerrainManager.cs
+++ Assets/Script/TerrainManager.cs
@@ -7,48 +7,38 @@ public class TerrainManager : MonoBehaviour
     public ObstacleGenerator obstacleGenerator;
     public TerrainPool terrainPool;
 
-    public List<TerrainData> terrainDatas = new List<TerrainData>();
-    public Vector3 previousTerrainEndPoint=Vector3.zero; // endPoint알아내기용
+    //public List<TerrainData> terrainDatas = new List<TerrainData>();
+    private Dictionary<TerrainType, GameObject> terrainDatas = new();
+    private Queue<GameObject> terrainQueue = new();
+    private Vector3 previousTerrainEndPoint=Vector3.zero; // endPoint알아내기용
+    [SerializeField] private Vector3 terrainOffset;
     private float time;
 
     void Start()
     {
         TerrainGenerator();
     }
+    private void InitTerrainData()
+    {
+        terrainDatas[TerrainType.Terrain1] = Resources.Load("Prefab/Terrain/Terrain1") as GameObject;
+    }
 
     public void TerrainGenerator()
     {
         TerrainType selectedTerrain = (TerrainType)UnityEngine.Random.Range(0, System.Enum.GetValues(typeof(TerrainType)).Length);
+        Vector3 nextTerrainPos = previousTerrainEndPoint + terrainOffset;
 
 
         switch (selectedTerrain)
         {
-            case TerrainType.Plane1:
+            /*case TerrainType.Plane1:
                 {
                     //Vector3 terrainPoint = previousTerrainEndPoint - terrainDatas[0].terrainStartLocalPoint;
                     Terrain Plane1 = terrainPool.GetTerrain(selectedTerrain);
                    // Plane1.transform.position = terrainPoint;
                     //previousTerrainEndPoint = Plane1.transform.TransformPoint(terrainDatas[0].terrainEndLocalPoint);
                 }
-                break;
-
-            case TerrainType.Plane2:
-                {
-                    //Vector3 terrainPoint = previousTerrainEndPoint - terrainDatas[1].terrainStartLocalPoint;
-                    Terrain Plane2 = terrainPool.GetTerrain(selectedTerrain);
-                   // Plane2.transform.position = terrainPoint;
-                   // previousTerrainEndPoint = Plane2.transform.TransformPoint(terrainDatas[1].terrainEndLocalPoint);
-                }
-                break;
-
-            case TerrainType.Steep1:
-                {
-                    //Vector3 terrainPoint = previousTerrainEndPoint - terrainDatas[2].terrainStartLocalPoint;
-                    Terrain Steep1 = terrainPool.GetTerrain(selectedTerrain);
-                  //  Steep1.transform.position = terrainPoint;
-                    //previousTerrainEndPoint = Steep1.transform.TransformPoint(terrainDatas[2].terrainEndLocalPoint);
-                }
-                break;
+                break;*/
         }
         
     }
