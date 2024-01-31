27e0b749d30e529feb122635f029f17a1cba0ea0
김은서 <94689360+KES123450@users.noreply.github.com>
2024-01-31 22:51:30 +0900

    [Feat] 동적 지형 생성 구현중
    지형프리팹 생성 위치 버그 고쳤음

--- Assets/Script/BezierMeshGenerator.cs
+++ Assets/Script/BezierMeshGenerator.cs
@@ -8,14 +8,13 @@ public class BezierMeshGenerator : MonoBehaviour
     public GeneratorMesh generatorMesh;
     public List<Vector3> bezierPointList;
 
-    public Vector3 p1; //테스트용
-    public Vector3 p2;
-    public Vector3 p3;
-    public Vector3 p4;
+    public Vector3 p1 { get; set; }
+    public Vector3 p2 { get; set; }
+    public Vector3 p3 { get; set; }
+    public Vector3 p4 { get; set; }
     public int pointNum;
     public string meshName;
 
-    public bool smoothMode;
     public void CreateBezierMesh()
     {
         GameObject ground = new GameObject();
@@ -28,6 +27,21 @@ public class BezierMeshGenerator : MonoBehaviour
         generatorMesh.generateMesh(ground,meshName);
         ground.AddComponent<TerrainData>();
         ground.GetComponent<TerrainData>().Init(p1, p2, p3, p4);
+    }
+
+    public GameObject CreateBezierMesh(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+    {
+        GameObject ground = new GameObject();
+        PolygonCollider2D polygonCollider2D = new PolygonCollider2D();
+
+        Vector2[] bezierPoints = bezierGenerator.GenerateBezierList(p1, p2, p3, p4, pointNum);
+        polygonCollider2D = ground.AddComponent<PolygonCollider2D>();
+        polygonCollider2D.points = bezierPoints;
+
+        generatorMesh.generateMesh(ground);
+        ground.AddComponent<TerrainData>();
+        ground.GetComponent<TerrainData>().Init(p1, p2, p3, p4);
+        return ground;
     }
 
 }
